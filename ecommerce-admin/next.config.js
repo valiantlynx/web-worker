e55116732d8712f483b357863fde58ea356b76ab
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-14 08:52:08 +0100

    Squashed 'apps/ecommerce/' changes from 92cf171..c4344a9
    c4344a9 Update next.config.js
    
    git-subtree-dir: apps/ecommerce
    git-subtree-split: c4344a9582db37140b0077aa8b68d6bc49a88d24

--- ecommerce-admin/next.config.js
+++ ecommerce-admin/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGODB_URI: 'mongodb+srv://usename:password@cluster0.lujhkmj.mongodb.net/?retryWrites=true&w=majority',
-    GOOGLE_ID: '794340205891-lnik4lr1kcnk6ov3ofh3s553v4dkrq26.apps.googleusercontent.com',
-    GOOGLE_SECRET: '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',
-    S3_ACCESS_KEY: 'AKIAY2SEWYAESK6ZZAGF',
-    S3_SECRET_ACCESS_KEY: '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',
-  },
 }
 
 module.exports = nextConfig
